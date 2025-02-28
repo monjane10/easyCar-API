6c53e70b628e61ebaeed7b4a752008acaeaab785
monjane10 <louramonja52.n@gmail.com>
2025-02-28 22:57:25 +0200

    ajustando o index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,14 @@ app.use(cors());
 //ROUTES
 app.get('/rides',controllerRide.List);
 app.post('/rides',controllerRide.Insert);
+app.delete("/rides/:ride_id", controllerRide.Delete);
+app.get("/rides/:ride_id", controllerRide.ListDetail);
+
+app.put("/rides/:ride_id/finish", controllerRide.Finish);
+app.get("/rides/drivers/:driver_user_id", controllerRide.ListForDriver);
+app.put("/rides/:ride_id/accept", controllerRide.Accept);
+app.put("/rides/:ride_id/cancel", controllerRide.Cancel);
+
 
 
 app.listen(3000, () => {
