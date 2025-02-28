e2d99106049971bc1d3a2cd74f132ec615032f94
monjane10 <louramonja52.n@gmail.com>
2025-02-28 22:53:05 +0200

    Ajustando o controlador

--- src/controllers/ridesController.js
+++ src/controllers/ridesController.js
@@ -27,16 +27,94 @@ async function Insert (req, res){
         const pickup_longitude = req.body.pickup_longitude;
         const dropoff_address = req.body.dropoff_address;
 
-        const rides = await serviceRide.Insert(passenger_user_id, pickup_address,
+        const rides = await rideService.Insert(passenger_user_id, pickup_address,
             pickup_latitude, pickup_longitude, dropoff_address);
 
         res.status(201).json(rides);
     } catch (error) {
         res.status(500).json({ error });
     }
-
- 
 }
 
+async function Delete(req, res) {
 
-export default {List, Insert};
+    try {
+        const ride_id = req.params.ride_id;
+
+        const ride = await rideService.Delete(ride_id);
+
+        res.status(200).json(ride);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+}
+
+async function Finish(req, res) {
+
+    try {
+        const ride_id = req.params.ride_id;
+        const passenger_user_id = req.body.passenger_user_id;
+
+        const ride = await rideService.Finish(ride_id, passenger_user_id);
+
+        res.status(200).json(ride);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+}
+
+async function ListForDriver(req, res) {
+
+    try {
+
+        const driver_user_id = req.params.driver_user_id;
+
+        const rides = await rideService.ListForDriver(driver_user_id);
+
+        res.status(200).json(rides);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+}
+
+async function ListDetail(req, res) {
+
+    try {
+        const ride_id = req.params.ride_id;
+
+        const rides = await rideService.List(null, null, ride_id, null, null);
+
+        res.status(200).json(rides[0]);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+}
+
+async function Accept(req, res) {
+
+    try {
+        const ride_id = req.params.ride_id;
+        const driver_user_id = req.body.driver_user_id;
+
+        const ride = await rideService.Accept(ride_id, driver_user_id);
+
+        res.status(200).json(ride);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+}
+
+async function Cancel(req, res) {
+
+    try {
+        const ride_id = req.params.ride_id;
+
+        const ride = await rideService.Cancel(ride_id);
+
+        res.status(200).json(ride);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+}
+
+export default { List, Insert, Delete, Finish, ListForDriver, ListDetail, Accept, Cancel };
