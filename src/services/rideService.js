0cf68f883ee1a59d826333d5ef615ba93eb1ee97
monjane10 <louramonja52.n@gmail.com>
2025-03-01 20:09:08 +0200

    adicionando validacoes

--- src/services/rideService.js
+++ src/services/rideService.js
@@ -8,6 +8,16 @@ return rides;
 async function Insert(passenger_user_id, pickup_address,
     pickup_latitude, pickup_longitude, dropoff_address) {
 
+        //Validação: O passageiro só pode pedir uma corrida por vez!
+        const dt = new Date().toISOString("pt-MZ", {
+            timeZone: "Africa/Maputo"}).substring(0, 10);
+            const searchRides =  await List(passenger_user_id, dt, null, null, null, "F");
+
+            if (searchRides.length > 0) {
+                throw "Você tem uma corrida não finalizada para o dia de hoje!";
+            }
+        //----------------------------------------------------------------
+
     const ride = await rideRepository.Insert(passenger_user_id, pickup_address,
         pickup_latitude, pickup_longitude, dropoff_address);
 
@@ -37,6 +47,14 @@ async function ListForDriver(driver_user_id) {
 
 async function Accept(ride_id, driver_user_id) {
 
+      //Validação: O motorista só pode pedir uma corrida por vez!
+      const dt = new Date().toISOString("pt-MZ", {
+        timeZone: "Africa/Maputo"}).substring(0, 10);
+      const searchRides =  await List(null, dt, null, driver_user_id, "A", null);
+      if (searchRides.length > 0) {
+        throw "Você tem uma corrida não finalizada para o dia de hoje:" + searchRides[0].passenger_user_id;
+    }
+      //---------------------------------------------
     const ride = await rideRepository.Accept(ride_id, driver_user_id);
 
     return ride;
